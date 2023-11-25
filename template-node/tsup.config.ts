65b2072eb3bf0a31fd1687e11072bca0ba3ab15a
tom <tom@tomgao.cc>
2023-11-25 22:08:21 +0800

    chore: update deps

--- template-node/tsup.config.ts
+++ template-node/tsup.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  target: ['es2022', 'node18'],
-  clean: true,
-  dts: true,
-  sourcemap: false,
-  splitting: true,
-});
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    target: ['es2022', 'node18'],
+    dts: false,
+  },
+  {
+    entry: ['src/index.ts'],
+    format: ['cjs'],
+    target: ['es2022', 'node18'],
+    dts: true,
+  },
+]);
