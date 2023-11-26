d8068e88f89ae6a1d82916bda0129949c81499da
tom <tom@tomgao.cc>
2023-11-26 20:30:12 +0800

    ci: change build config

--- template-node/tsup.config.ts
+++ template-node/tsup.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
-    format: ['esm'],
-    target: ['es2020', 'node16'],
-    dts: false,
-  },
-  {
-    entry: ['src/index.ts'],
-    format: ['cjs'],
-    target: ['es2020', 'node16'],
-    dts: true,
-    splitting: true,
-  },
-]);
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  target: ['es2021', 'node16'],
+  clean: true,
+  dts: true,
+  sourcemap: false,
+  splitting: true,
+});
