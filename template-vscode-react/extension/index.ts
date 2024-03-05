21a76872cf100f42368b870a2f7e98d4d43f8028
Tom Gao <tom@tomgao.cc>
2024-03-05 12:22:08 +0800

    feat: vscode template code

--- template-vscode-react/extension/index.ts
+++ template-vscode-react/extension/index.ts
@@ -2,10 +2,9 @@ import { commands, ExtensionContext } from 'vscode';
 import { MainPanel } from './views/panel';
 
 export function activate(context: ExtensionContext) {
-  // Add command to the extension context
   context.subscriptions.push(
     commands.registerCommand('hello-world.showHelloWorld', async () => {
-      MainPanel.render(context);
+      MainPanel.render(context.extensionUri);
     }),
   );
 }
--- template-vscode/src/index.ts
+++ template-vscode/src/index.ts
@@ -1,13 +1,11 @@
 import { commands, ExtensionContext, window } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-  // Create the show hello world command
-  const showHelloWorldCommand = commands.registerCommand('hello-world.showHelloWorld', async () => {
-    window.showInformationMessage('Hello World');
-  });
-
-  // Add command to the extension context
-  context.subscriptions.push(showHelloWorldCommand);
+  context.subscriptions.push(
+    commands.registerCommand('hello-world.showHelloWorld', async () => {
+      window.showInformationMessage('Hello World');
+    }),
+  );
 }
 
 export function deactivate() {}
